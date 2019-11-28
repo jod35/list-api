00ad3988d328390d1e01df08b5e8a81493667be5
jod35 <jodestrevin@gmail.com>
2019-11-28 20:33:30 +0300

    update request that changes data for a specific task

--- app.py
+++ app.py
@@ -56,8 +56,26 @@ def create_tasks():
     }
     tasks.append(task)
     return jsonify({'task':task}),201
-    
 
+@app.route('/tasks/<int:task_id>',methods=['PUT'])
+def update_task(task_id):
+    task=[task for task in tasks if task['id']==task_id]
+    if len(task) ==0:
+        abort(404)
+    if not request.json:
+        abort(404)
+    if 'title' in request.json and type(request.json['title']) != str:
+        abort(404)
+    if 'description' in request.json and type(request.json['description']) is not str:
+        abort(404)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(404)
+
+    task[0]['title']=request.json.get('title',task[0]['title'])
+    task[0]['description']=request.json.get('description',task[0]['description'])
+    task[0]['done']=request.json.get('done',task[0]['done'])
+
+    return jsonify({'task':task[0]})     
 
 if __name__ == "__main__":
     app.run(debug=True)
